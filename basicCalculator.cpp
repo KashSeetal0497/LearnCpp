a5c682a483836e8d02a49da952173366bcbe51c5
JarushanNaidoo <jarushan.naidoo7@gmail.com>
2020-10-01 00:42:46 +0200

    Initial Commit

---
+++ basicCalculator.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+namespace calculator {
+	class operations{
+	public:
+		int addition(const int a, const int b) {
+			return a + b;
+		}
+
+		int subtraction(const int a, const int b) {
+			if (a > b) return a - b;
+			return b - a;
+		}
+
+		int multiplication(int a, const int b) {
+			int result = 0;
+			for (int i = 0; i < (double)b/2; i++) {
+				result += addition(a, a);
+			}
+
+			return result;
+		}
+
+		float division(const float a, const float b) {
+			if (a < b) return (b / a);
+			return (a / b);
+		}
+	};
+
+	class Calculator : public operations {
+	public:
+		int a;
+		int b;
+
+		Calculator(int a, int b) {
+			a = a;
+			b = b;
+		}
+
+
+		void demo() {
+			while (cin.get() != 'q') {
+				cout << "Enter a" << endl;
+				cin >> a;
+				cout << "Enter b" << endl;
+				cin >> b;
+
+				cout << "Addition: " << addition(a, b) << endl;
+
+				cout << "Subtraction: " << subtraction(a, b) << endl;
+
+				cout << "Multiplication: " << multiplication(a, b) << endl;
+
+				float x = (float)a;
+				float y = (float)b;
+
+				cout << "Division: " << division(x, y) << endl;
+
+				cout << "q to exit, anything else to continue " << endl;
+				cout << endl;
+				cin.get();
+
+			}
+		}
+	};
+};
+
+
+using namespace calculator;
+int main() {
+	Calculator c = Calculator(0,0);
+	c.demo();
+	
+	return 0;
+}
